8acec6167e54e8dcd3564f6226ad819d3f426cb7
leite <leitesdzn@gmail.com>
2021-04-29 10:08:37 +0100

    added double javascript regression

--- main.js
+++ main.js
@@ -2,13 +2,15 @@ var x = 1;
 var y = null; // To keep under proper scope
 
 setTimeout(function() {
-    x = x * 3 + 2;
-    y = x / 2;
-    document.getElementById("delay").innerHTML = "Delay Working";
-    var x = document.createElement("IMG");
-  x.setAttribute("src", "4.jpg");
-  x.setAttribute("width", "304");
-  x.setAttribute("height", "228");
-  x.setAttribute("alt", "The Pulpit Rock");
-  document.body.appendChild(x);
-}, 5000);
+    // Get the head tag
+    var head_ID = document.getElementsByTagName("head")[0]; 
+    // Create script element       
+    var script_element = document.createElement('script');
+    // Set the script type to JavaScript
+    script_element.type = 'text/javascript';
+    // External JS file
+    script_element.src = 'javas.js';
+    head_ID.appendChild(script_element);
+}, 2000);
+
+
